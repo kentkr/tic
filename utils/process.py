a8ea5bfb320ad737852fbe3cb6793f3ce06956d9
Kyle Kent <kylekent@Kyles-MacBook-Pro-2.local>
2022-12-26 13:43:18 -0700

    shifting over to processing with ijson, this commit gets the json file structure so I can plan parsing it

--- utils/process.py
+++ utils/process.py
@@ -26,7 +26,7 @@ def recursive_expand(df):
             df = df.join(normalized_df,
                     lsuffix = '_left',
                     rsuffix = '_right')
-        # if
+        # if col is list explode
         elif df[col_name].apply(pd.api.types.is_list_like).any(): 
             print('=== expanding ===')
             df = df.explode(col_name)
@@ -59,11 +59,18 @@ def combine_jsons(dir_path):
     allowed_amounts = pd.DataFrame()
     
     for file in files:
-        print(file)
+        # if ds store skip, should be changed to if tolower('.json')
         if file == '.DS_Store':
-            print('skipping')
             continue
+        # get file path
         file_path = os.path.join(dir_path, file)
+
+        # if file larger than 1GB skip
+        if os.path.getsize(file_path) > 1e+9: 
+            print('File too large, bytes: {}, skipping'.format(os.path.getsize(file_path)))
+            continue
+
+        # open file and load as json
         with open(file_path, 'r') as file_:
             jsoned = json.load(file_)
 
@@ -74,3 +81,49 @@ def combine_jsons(dir_path):
     allowed_amounts = allowed_amounts.reset_index(drop = True)
     return allowed_amounts
 
+# get json structure (outputs list)
+def get_json_structure(file_path):
+    # open file as bytes
+    with open(file_path, 'rb') as f:
+        # instantiate parser
+        parser = ijson.parse(f, use_float = True)
+        # unique set of prefixes
+        prefixes = set()
+        # for each element add prefix to set
+        for prefix, event, value in parser:
+            prefixes.add(prefix)
+
+    # remove empty string
+    prefixes.remove('')
+
+    # sort values by length
+    prefixes = sorted(prefixes, key = len)
+    return prefixes
+
+# pretty print the file structure
+def print_structure(values):
+    # Create a nested dictionary to store the values and their relationships
+    structure = {}
+    for value in values:
+        # Split the value into a list of path components
+        path = value.split(".")
+        # Use the path components to build the nested dictionary
+        current = structure
+        for component in path[:-1]:
+            current = current.setdefault(component, {})
+        current[path[-1]] = {}
+    # Define a recursive function to print the structure
+    def print_recursive(structure, indent):
+        # Iterate over the keys and values in the structure
+        for i, (key, value) in enumerate(structure.items()):
+            # Print the appropriate characters before the key
+            if i == 0:
+                # If this is the first key, print a line
+                print(" " * indent + " - " + key)
+            else:
+                # If this is not the first key, print a dash and a space
+                print(" " * indent + " - " + key)
+            # Recursively print the structure of the value
+            print_recursive(value, indent + 2)
+    # Call the recursive function to print the structure
+    print_recursive(structure, 0)
